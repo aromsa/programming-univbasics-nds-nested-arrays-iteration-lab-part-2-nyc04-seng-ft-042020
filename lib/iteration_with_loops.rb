19dc0a0cb8aca0e66c529b8ea38f410e206890e5
anna bateman <annacbateman@gmail.com>
2020-04-09 18:54:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,6 +9,7 @@ min_array = []
 min = nil
 while row_index < src.count do
   element_index = 0
+  binding.pry
   while element_index < src[row_index].count do
      if min == nil ||
        src[row_index][element_index] < min
